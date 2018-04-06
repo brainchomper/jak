60aa87a4a9260ce9235ea6374eef22c043c797ff
Kevin Smith <kevin@kvnsmith.com>
2018-04-05 19:59:57 -0600

    weather and user

--- UserSearch.js
+++ UserSearch.js
@@ -1,8 +1,12 @@
 var weather = require('weather-js');
+var moment = require('moment');
+var now = moment();
 
 var UserSearch = function (name, location) {
 	this.name = name;
 	this.location = location;
+	this.date = Date.now();
+
 	this.getWeather = function () {
 		weather.find({ search: this.location, degreeType: 'F' }, function (err, result) {
 			if (err) console.log(err);
